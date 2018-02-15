04c627af36d45c5ebfe8271e5895721eaa4f76cc
Lee Lerner <lee.lerner@uncorkedstudios.com>
2018-02-15 15:05:50 -0800

    added GPL text to arduino sketches

--- SimoneGame/SimoneGame.ino
+++ SimoneGame/SimoneGame.ino
@@ -1,19 +1,28 @@
 /*
- * Original Author: Denis Cosmin
- * Date: 19.09.2016
- * 
- * Updated Author: Lee Lerner at Uncorked Studios
- * Date: January 20th 2018
+   Original Author: Denis Cosmin
+   Date: 19.09.2016
+  
+   Updated Author: Lee Lerner at Uncorked Studios
+   Date: January 20th 2018
+   Copyright (C) 2018  Uncorked Studios lee@uncorkedstudios.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  * 
  * To add a reset button connect: reset -> button <- pulldown resistor ground.
  * The buttons have 1k pulldown resitors.
  * The leds have a 220 ohm resistor.
- * 
- * Will use the following numbers for colors, pins and notes
- * 0 - Yellow
- * 1 - Green
- * 2 - Red
- * 3 - Blue
  * 
  */
 
--- SimoneTest/SimoneTest.ino
+++ SimoneTest/SimoneTest.ino
@@ -5,6 +5,22 @@
    About: Test software to be used during build for Simone game for troubleshooting purposes
    Use: When wired properly, each button should light up when pressed
    When all buttons lit, SimoneGame.ino is ready to be uploaded
+
+
+   Copyright (C) 2018  Uncorked Studios lee@uncorkedstudios.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //test stuff
